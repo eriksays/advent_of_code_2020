8e0b699bc2c92f849faa1d4ea136df6477e7542c
matthew collins <mcollins@wiley.com>
2020-12-05 11:43:41 -0500

    add a third layer

--- day1/main.py
+++ day1/main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def main():
+def main(sums=2):
     #open expenses file and convert to list    
     expense_file = pd.read_csv('expenses.csv', header=None, names=["expenses"])
     expenses = expense_file["expenses"].to_list()
@@ -13,12 +13,20 @@ def main():
     for x in expenses:
         #and check against each other number
         for y in expenses:
-
-            if int(x) + int(y) == sum:
-                print("x:{} + y: {} = {}".format(x,y,sum))
-                print("final answer: {}".format(x*y))
-                exit()
+            if sums == 2:
+                if int(x) + int(y) == sum:
+                    print("x:{} + y: {} = {}".format(x,y,sum))
+                    print("final answer: {}".format(x*y))
+                    exit()
+            else:
+                for z in expenses:
+                   if int(x) + int(y) + int(z) == sum:
+                    print("x:{} + y:{} + z:{} = {}".format(x,y,z,sum))
+                    print("final answer: {}".format(x*y*z))
+                    exit() 
 
 
 if __name__ == "__main__":
-    main()
+    #if we're only adding two numbers, leave main() blank, 
+    #else, add in 3 for a third loop
+    main(3)
